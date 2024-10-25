d0d7a95c9202263e9418cf07e356fe09d10c7186
Ruan Manoel <ruuanmanoel@gmail.com>
2024-10-24 21:47:51 -0300

    :sperkles: implementando o hitbox

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -7,7 +7,9 @@ const state ={
     },
     values:{
         timerId: null,
-        gameVelocity: 1000
+        gameVelocity: 1000,
+        hitPosition:0,
+        result: 0
     }
 }
 function randomSquare(){
@@ -17,16 +19,25 @@ function randomSquare(){
     let randomNumber = Math.floor(Math.random() * 9);
     let randomSquare = state.view.squares[randomNumber];
     randomSquare.classList.add('enemy')
+    state.values.hitPosition = randomSquare.id;
 }
 function moveEnemy(){
     state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
 }
 function addListenerBox(){
     state.view.squares.forEach((square)=>{
+        square.addEventListener('mousedown', ()=>{
+            if(square.id === state.values.hitPosition){
+                state.values.result++;
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+            }
+        })
     })
 }
 function init(){
-   moveEnemy();
+    moveEnemy();
+   addListenerBox();
 }
 
 init();
